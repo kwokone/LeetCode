89dd88eb1e407bb520d4da98554a743be41eabbc
Allen <Allen@ALLEN-PC>
2023-03-10 04:21:49 +0800

    :tada:Add 0001

---
+++ easy/src/_0001/TwoSum/Solution.java
@@ -0,0 +1,18 @@
+package _0001.TwoSum;
+
+/**
+ * 📆️ 2023/3/9
+ * <p>
+ * ✅ TODO
+ * ❎ FIXME
+ *
+ * @author Allen
+ * @version 0.0.1
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target){
+
+
+    }
+}
