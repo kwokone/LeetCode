0ca20e6d0a7b68dc4912a53f2faa0bf7b9c13de6
Allen <Allen@ALLEN-PC>
2023-03-10 23:21:19 +0800

    :tada:TODO 最长回文字符串

--- medium/src/main/java/com/app/leetcode/_0005/Solution.java
+++ medium/src/main/java/com/app/leetcode/_0005/Solution.java
@@ -3,7 +3,8 @@ package com.app.leetcode._0005;
 /**
  * 📆️ 2023/3/10
  * 最长回文子串
- * ✅ TODO
+ * ✅ 动态规划是为了减少重复计算的问题
+ * 动态规划的关键是找到初始状态和状态转移方程
  * ❎ FIXME
  *
  * @author Allen
@@ -12,7 +13,11 @@ package com.app.leetcode._0005;
 public class Solution {
 
     public String longestPalindrome(String s) {
+        if(s == null || s.length() < 2) return s;
 
+        int len = s.length(); // 字符串的长度
+        int maxStart = 0; // 最长回文串的起点
+        int
 
         return null;
     }
